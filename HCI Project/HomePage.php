7ccec5977dd25b456b791c30a551d381e9ede6ea
MichelleJames03 <54328411+MichelleJames03@users.noreply.github.com>
2020-05-14 20:14:29 +0300

    Add files via upload

---
+++ HCI Project/HomePage.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>KFC DELIVERY</title>
+	<link rel="stylesheet" href="HP.css">
+
+<header>
+	<p>KFC DELIVERY</p>
+
+</header>
+</head>
+<body>
+<div class="topnav">
+  <a class="active" href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
+</div>
+
+
+</body>
+</html>
